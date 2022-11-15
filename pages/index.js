3d681c3e49695ef4462ec21cb158c2af9026f034
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2022-11-15 12:28:52 -0800

    i chakra

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,30 @@ import Head from "next/head";
 import Name from "../components/Name";
 import Tagline from "../components/Tagline";
 import Image from "../components/Image";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function Home() {
 	return (
 		<div>
-			<Head>
-				<title>Generate Startup Names</title>
-			</Head>
-			<main className="main">
-				<h1>
-					{" "}
-					🦄🎉 Congrats on your decision to become a founder 🎉🦄{" "}
-				</h1>
-				<h2>
-					{" "}
-					Fasten your seatbelt! You'll need to a name, a tagline and a
-					meme for your new startup.
-				</h2>
-				<Name />
-				<Tagline />
-				<Image />
-			</main>
+			<ChakraProvider>
+				<Head>
+					<title>Generate Startup Names</title>
+				</Head>
+				<main className="main">
+					<h1>
+						{" "}
+						🦄🎉 Congrats on your decision to become a founder 🎉🦄{" "}
+					</h1>
+					<h2>
+						{" "}
+						Fasten your seatbelt! You'll need to a name, a tagline
+						and a meme for your new startup.
+					</h2>
+					<Name />
+					<Tagline />
+					<Image />
+				</main>
+			</ChakraProvider>
 		</div>
 	);
 }
